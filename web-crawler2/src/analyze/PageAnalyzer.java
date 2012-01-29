83e5778a230432622c241bbfbc0c87a4671b4f36
j@jpgunter.com <j@jpgunter.com@9c9c3aee-6783-2d49-9dd9-4d23308f74a0>
2012-01-29 22:51:48 +0000

    Started implementing the parse and analyze code.

--- web-crawler2/src/analyze/PageAnalyzer.java
+++ web-crawler2/src/analyze/PageAnalyzer.java
@@ -1,5 +1,42 @@
 package analyze;
 
-public class PageAnalyzer {
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
+import page.Page;
+import statistics.Statistics;
+
+public class PageAnalyzer {
+	private BlockingQueue<Page> my_pages_to_analyze;
+	private BlockingQueue<Page> my_completed_pages;
+	
+	private Statistics my_stats;
+
+	public PageAnalyzer(BlockingQueue<Page> the_pages_to_analyze,
+      BlockingQueue<Page> the_completed_pages, Statistics the_stats) {
+	  my_pages_to_analyze = the_pages_to_analyze;
+	  my_completed_pages = the_completed_pages;
+	  my_stats = the_stats;
+  }
+	
+	public void analyze(){
+		Page a_page;
+		try{
+			a_page = my_pages_to_analyze.poll(1, TimeUnit.SECONDS);
+		}catch(InterruptedException e){
+			//Do nothing, we probably want to stop.
+			a_page = null;
+		}
+		if(a_page != null){
+			
+			
+			try{
+				my_completed_pages.put(a_page);
+			}catch (InterruptedException e1) {
+				//Do nothing, we probably want to stop.
+			}
+		}
+	}
+	
+	
 }
--- web-crawler2/src/parse/PageParser.java
+++ web-crawler2/src/parse/PageParser.java
@@ -1,5 +1,83 @@
 package parse;
 
-public class PageParser {
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.HTMLElementName;
+import net.htmlparser.jericho.Source;
+import page.Page;
+import statistics.Statistics;
+
+public class PageParser {
+	private BlockingQueue<Page> my_pages_to_parse;
+	private BlockingQueue<Page> my_pages_to_retrieve;
+	private BlockingQueue<Page> my_pages_to_analyze;
+	
+	private Statistics my_stats;
+
+	public PageParser(BlockingQueue<Page> the_pages_to_parse,
+      BlockingQueue<Page> the_pages_to_retrieve,
+      BlockingQueue<Page> the_pages_to_analyze, Statistics the_stats) {
+	  my_pages_to_parse = the_pages_to_parse;
+	  my_pages_to_retrieve = the_pages_to_retrieve;
+	  my_pages_to_analyze = the_pages_to_analyze;
+	  my_stats = the_stats;
+  }
+	
+	public void parse(){
+		Page a_page;
+    try {
+	    a_page = my_pages_to_parse.poll(1, TimeUnit.SECONDS);
+    } catch (InterruptedException e1) {
+    	//Do nothing, we probably want to stop.
+	    a_page = null;
+    }
+		if(a_page != null){
+			Source page_source = new Source(a_page.getContents());
+
+			page_source.fullSequentialParse();
+
+			List<Element> linkElements = page_source
+					.getAllElements(HTMLElementName.A);
+			for (Element linkElement : linkElements) {
+				String href = linkElement.getAttributeValue("href");
+				if (href == null)
+					continue;
+
+				if (href.endsWith(".html") || href.endsWith(".txt")) {
+					a_page.addLink(a_page.getAddress().resolve(href).toString());
+				}
+			}
+
+			String page_text = page_source.getTextExtractor().toString();
+
+			Pattern word_pat = Pattern.compile("\\b(\\w+)\\b");
+			Matcher word_mat = word_pat.matcher(page_text);
+			
+			while (word_mat.find()) {
+				a_page.addWord(word_mat.group(1));
+			}
+			
+			for(String s:a_page.getLinks()){
+				try {
+	        my_pages_to_retrieve.put(new Page(new URI(s)));
+	        my_pages_to_analyze.put(a_page);
+        } catch (InterruptedException e) {
+	        //Do nothing, we probably want to stop.
+        } catch (URISyntaxException e) {
+	        //Invalid url, just skip it.
+        }
+			}
+			
+			
+		}
+		
+	}
+	
 }
